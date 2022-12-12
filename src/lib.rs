f4dc922652b39d7992bb8bef43cd79f1f6bef629
Ikenna Umeh <kennaumeh@gmail.com>
2022-12-12 16:39:03 +0100

    Added environment and case sensitive features

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,25 @@
 use std::error::Error;
 use std::fs;
+use std::env;
+
+pub struct  Config{
+    pub query : String,
+    pub file_path : String,
+    pub ignore_case : bool,
+}
 
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
     Ok(())
@@ -19,16 +32,14 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
     
-        Ok(Config { query, file_path})
+        Ok(Config { query, file_path, ignore_case})
     
     }
 }
 
-pub struct  Config{
-    pub query : String,
-    pub file_path : String
-}
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
     let mut results = Vec::new();
@@ -41,12 +52,36 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);            
+        }        
+    }
+
+    results
+}
 #[cfg(test)]
 mod tests {
     use  super::*;
 
     #[test]
-    fn one_result(){
+    fn case_sensitive(){
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive(){
         let query = "duct";
         let contents = "\
 Rust:
