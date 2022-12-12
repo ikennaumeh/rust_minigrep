4a716c370c75e9bce83f07c8ed843cbe9a0219b3
Ikenna Umeh <kennaumeh@gmail.com>
2022-12-12 16:00:08 +0100

    Written Tests

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("\nWith text: \n{contents}",);
-
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
@@ -27,4 +28,32 @@ impl Config {
 pub struct  Config{
     pub query : String,
     pub file_path : String
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+
+    for line in contents.lines(){
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use  super::*;
+
+    #[test]
+    fn one_result(){
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,7 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
-    print!("Search for {}", config.query);
-    print!("\nIn file {}", config.file_path);
+    
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
